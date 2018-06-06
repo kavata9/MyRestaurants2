3b1b582c18f91f086cb5a7fb85f3495a4dee96f5
agnes musyoka <kavatamusyoka24@gmail.com>
2018-06-06 17:45:15 +0300

    add tosters

--- app/src/main/java/com/restaurants/agnes/myrestaurants/MainActivity.java
+++ app/src/main/java/com/restaurants/agnes/myrestaurants/MainActivity.java
@@ -2,12 +2,24 @@ package com.restaurants.agnes.myrestaurants;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    private Button mFindRestaurantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
