91ed066c12f4fc3b68cfd980065081b5920268a8
agnes musyoka <kavatamusyoka24@gmail.com>
2018-06-06 21:34:37 +0300

    add new activity restaurantss

--- app/src/main/java/com/restaurants/agnes/myrestaurants/MainActivity.java
+++ app/src/main/java/com/restaurants/agnes/myrestaurants/MainActivity.java
@@ -1,10 +1,10 @@
 package com.restaurants.agnes.myrestaurants;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
     private Button mFindRestaurantsButton;
@@ -13,12 +13,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
         mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/restaurants/agnes/myrestaurants/RestaurantsActivity.java
@@ -0,0 +1,13 @@
+package com.restaurants.agnes.myrestaurants;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RestaurantsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+    }
+}
