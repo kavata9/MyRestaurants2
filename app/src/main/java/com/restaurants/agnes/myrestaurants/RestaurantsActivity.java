44827dd1e7c7957cd36f80eb547884eb3a67d7b8
agnes musyoka <kavatamusyoka24@gmail.com>
2018-06-06 22:18:49 +0300

    add function on restaurants activity to passdata with extra intent and also location

--- app/src/main/java/com/restaurants/agnes/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/restaurants/agnes/myrestaurants/RestaurantsActivity.java
@@ -1,13 +1,20 @@
 package com.restaurants.agnes.myrestaurants;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RestaurantsActivity extends Activity {
+    private TextView mLocationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Here are all the restaurants near: " + location);
     }
 }
