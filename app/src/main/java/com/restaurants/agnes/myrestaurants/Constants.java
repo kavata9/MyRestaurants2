62f555a6f86feb2a544a5668e56206a4bee7a99c
agnes musyoka <kavatamusyoka24@gmail.com>
2018-06-11 10:48:36 +0300

    add okhttp

---
+++ app/src/main/java/com/restaurants/agnes/myrestaurants/Constants.java
@@ -0,0 +1,7 @@
+package com.restaurants.agnes.myrestaurants;
+
+public class Constants {
+    public static final String YELP_API_KEY = BuildConfig.YELP_API_KEY;
+    public static final String YELP_BASE_URL = "https://api.yelp.com/v2/search?term=food";
+    public static final String YELP_LOCATION_QUERY_PARAMETER = "location";
+}
---
+++ app/src/main/java/com/restaurants/agnes/myrestaurants/YelpService.java
@@ -0,0 +1,13 @@
+package com.restaurants.agnes.myrestaurants;
+
+
+import okhttp3.Callback;
+
+
+public class YelpService {
+
+    public static void findRestaurants(String location, Callback callback) {
+
+    }
+
+}
